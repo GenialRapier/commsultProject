e3a46eb4f23258bbfadf1681720fab93ec5d74e9
ahmad.windardi80 <ahmad.windardi80@gmail.com>
2018-10-06 10:53:39 +0700

    Add MainController and Actors class

---
+++ src/main/java/id/ac/sgu/commsultProject/Actors.java
@@ -0,0 +1,8 @@
+package id.ac.sgu.commsultProject;
+
+public class Actors {
+	public void updateStatus() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/main/java/id/ac/sgu/commsultProject/MainController.java
@@ -0,0 +1,17 @@
+package id.ac.sgu.commsultProject;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class MainController implements PropertyChangeListener {
+	
+	public Double measurement;
+	public Actors actor;
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
