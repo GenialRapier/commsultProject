95bef7bfe91d71a04bccf9f48d5b6e920794c441
ahmad.windardi80 <ahmad.windardi80@gmail.com>
2018-10-06 10:47:02 +0700

    Add Anemometer and Clock class

---
+++ src/main/java/id/ac/sgu/commsultProject/Clock.java
@@ -0,0 +1,37 @@
+package id.ac.sgu.commsultProject;
+
+public class Clock implements Sensors {
+	
+	private Double measurement;
+
+	@Override
+	public void updateMeasurement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMeasurement(Double measurement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Double getMeasurement() {
+		// TODO Auto-generated method stub
+		return measurement;
+	}
+
+	@Override
+	public void addPropertyChangeListener() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removePropertyChangeListener() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
